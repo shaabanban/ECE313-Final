b100ad9155a2259b01718d84a1fc3fbd22ba0f81
Larry Resnik <LeftRightAB@gmail.com>
2014-11-29 14:09:11 -0600

    Made pre-task 3 plots render optionally. Removed junk files from repo.

--- task0.m
+++ task0.m
@@ -2,6 +2,7 @@
 % vim:noexpandtab tabstop=4
 close all;
 clear all;
+PLOT_PRE_TASK3_FIGURES =  0;
 patients=[];
 % Open the result file
 %fid = fopen('ECE313_Final_Project_group_zeta.txt', 'w');
@@ -42,113 +43,113 @@ labels_per_file = cell(1, NUM_PATIENTS);
 % Load all data files.
 %fprintf(fid, 'Task 0\n\n');
 for i = 1:NUM_PATIENTS
-load(filenames{i});
-all_data=floor(all_data);
-sizetraining=int32(size(all_data,2)*2./3.);
-sizetesting=sizetraining+1;
-sizetotal=size(all_data);
-
-
-patient=struct('all',all_data,'labels',all_labels,'trainingData', ...
-    struct('area', all_data(1,1:sizetraining),'rr',all_data(2,1:sizetraining), ...
-    'bpm',all_data(3,1:sizetraining),'p2p_bp',all_data(4,1:sizetraining), ...
-    'systolic',all_data(5,1:sizetraining),'diastolic',all_data(6,1:sizetraining)...
-    ,'pulse_pr',all_data(7,1:sizetraining),'golden',all_labels(1,1:sizetraining)),'trainingLabels',all_labels(1,1:sizetraining), ...
-    'testingData',struct('area', all_data(1,sizetesting:end),'rr',all_data(2,sizetesting:end), ...
-    'bpm',all_data(3,sizetesting:end),'p2p_bp',all_data(4,sizetesting:end), ...
-    'systolic',all_data(5,sizetesting:end),'diastolic',all_data(6,sizetesting:end)...
-    ,'pulse_pr',all_data(7,sizetesting:end),'golden',all_labels(1,sizetesting:end)),'testingLabels',all_labels(1,sizetesting:end));
-trgolden = [];
-trnongolden = [];
-gcntr=1;
-ngcntr=1;
-for j=1:sizetraining
-    if patient.trainingData.golden(j)==1
-        trgolden(1:7,gcntr)=patient.all(1:7,j);
-                        gcntr=gcntr+1;
-    else
-                trnongolden(1:7,ngcntr)=patient.all(1:7,j);
-                ngcntr=ngcntr+1;
+    load(filenames{i});
+    all_data=floor(all_data);
+    sizetraining=int32(size(all_data,2)*2./3.);
+    sizetesting=sizetraining+1;
+    sizetotal=size(all_data);
+    
+    
+    patient=struct('all',all_data,'labels',all_labels,'trainingData', ...
+        struct('area', all_data(1,1:sizetraining),'rr',all_data(2,1:sizetraining), ...
+        'bpm',all_data(3,1:sizetraining),'p2p_bp',all_data(4,1:sizetraining), ...
+        'systolic',all_data(5,1:sizetraining),'diastolic',all_data(6,1:sizetraining)...
+        ,'pulse_pr',all_data(7,1:sizetraining),'golden',all_labels(1,1:sizetraining)),'trainingLabels',all_labels(1,1:sizetraining), ...
+        'testingData',struct('area', all_data(1,sizetesting:end),'rr',all_data(2,sizetesting:end), ...
+        'bpm',all_data(3,sizetesting:end),'p2p_bp',all_data(4,sizetesting:end), ...
+        'systolic',all_data(5,sizetesting:end),'diastolic',all_data(6,sizetesting:end)...
+        ,'pulse_pr',all_data(7,sizetesting:end),'golden',all_labels(1,sizetesting:end)),'testingLabels',all_labels(1,sizetesting:end));
+    trgolden = [];
+    trnongolden = [];
+    gcntr=1;
+    ngcntr=1;
+    for j=1:sizetraining
+        if patient.trainingData.golden(j)==1
+            trgolden(1:7,gcntr)=patient.all(1:7,j);
+            gcntr=gcntr+1;
+        else
+            trnongolden(1:7,ngcntr)=patient.all(1:7,j);
+            ngcntr=ngcntr+1;
+        end;
     end;
-end;
     patient.trainingGolden=trgolden;
-   patient.trainingNonGolden=trnongolden;
-
-  trgolden = [];
-trnongolden = [];
-gcntr=1;
-ngcntr=1;
-for j=1:size(patient.testingData.golden,2)
-    k=(j+sizetesting-1);
-    if patient.testingData.golden(j)==1
-        trgolden(1:7,gcntr)=patient.all(1:7,k);
-          gcntr=gcntr+1;
-    else
-                trnongolden(1:7,ngcntr)=patient.all(1:7,k);
-                ngcntr=ngcntr+1;
+    patient.trainingNonGolden=trnongolden;
+    
+    trgolden = [];
+    trnongolden = [];
+    gcntr=1;
+    ngcntr=1;
+    for j=1:size(patient.testingData.golden,2)
+        k=(j+sizetesting-1);
+        if patient.testingData.golden(j)==1
+            trgolden(1:7,gcntr)=patient.all(1:7,k);
+            gcntr=gcntr+1;
+        else
+            trnongolden(1:7,ngcntr)=patient.all(1:7,k);
+            ngcntr=ngcntr+1;
+        end;
     end;
-end;
     patient.testingGolden=trgolden;
-   patient.testingNonGolden=trnongolden;
-   
-     patient.H1 = sum(patient.trainingLabels) / size(patient.trainingLabels, 2);
+    patient.testingNonGolden=trnongolden;
+    
+    patient.H1 = sum(patient.trainingLabels) / size(patient.trainingLabels, 2);
     % H1 is the probability that there is a patient abnomality.
-     patient.H0 = 1.0 -  patient.H1;
-  
-
-   training_matrix=[];
+    patient.H0 = 1.0 -  patient.H1;
+    
+    
+    training_matrix=[];
     testing_matrix=[];
-
-for j=1:7
-    training_matrix=[training_matrix,{ crosstab_f(patient.trainingGolden(j,:),patient.trainingNonGolden(j,:),patient.H0,patient.H1)}];
-    testing_matrix=[testing_matrix,{crosstab_f(patient.testingGolden(j,:),patient.testingNonGolden(j,:),patient.H0,patient.H1)}];
-end;
-
-   patient.testing_matrix=testing_matrix;
-   patient.training_matrix=training_matrix;
-   
- %calculate alarms
- alarms_ml=zeros(7,size(patient.testingData.area,2));
-  alarms_map=zeros(7,size(patient.testingData.area,2));
-  false_alarms_map=zeros(7,size(patient.testingData.area,2));
-  missed_alarms_map=zeros(7,size(patient.testingData.area,2));
- false_alarms_ml=zeros(7,size(patient.testingData.area,2));
-  missed_alarms_ml=zeros(7,size(patient.testingData.area,2));
-for j=1:7
-    current_test_data=all_data(j,sizetesting:end);
-   for k=1:size(patient.testingData.area,2)
-    [~,idx]=ismember([current_test_data(k)],patient.training_matrix{j}(:,1),'rows');  %ML
-    if idx~=0
-        alarms_ml(j,k)=patient.training_matrix{j}(idx,4);
-        alarms_map(j,k)=patient.training_matrix{j}(idx,5);
-    end
-    if patient.testingLabels(k) == 1
-        if alarms_ml(j,k) == 0   %missed alaram ml
-            missed_alarms_ml(j,k)=1;
-        end
-        if alarms_map(j,k) == 0  %missed alaram map
-              missed_alarms_map(j,k)=1;
-        end
-    else
-        if alarms_ml(j,k) == 1              %false alaram ml
-            false_alarms_ml(j,k)=1;
-        end
-        if alarms_map(j,k) == 1              %false alaram map
-            false_alarms_map(j,k)=1;
-        end
+    
+    for j=1:7
+        training_matrix=[training_matrix,{ crosstab_f(patient.trainingGolden(j,:),patient.trainingNonGolden(j,:),patient.H0,patient.H1)}];
+        testing_matrix=[testing_matrix,{crosstab_f(patient.testingGolden(j,:),patient.testingNonGolden(j,:),patient.H0,patient.H1)}];
     end;
-   end;
-end;
-patient.alarms_ml=alarms_ml;
-patient.alarms_map=alarms_map;
-patient.false_alarms_map=false_alarms_map;
-patient.false_alarms_ml=false_alarms_ml;
-patient.missed_alarms_map=missed_alarms_map;
-patient.missed_alarms_ml=missed_alarms_ml;
-
-
-patients=[patients,patient];
-
+    
+    patient.testing_matrix=testing_matrix;
+    patient.training_matrix=training_matrix;
+    
+    %calculate alarms
+    alarms_ml=zeros(7,size(patient.testingData.area,2));
+    alarms_map=zeros(7,size(patient.testingData.area,2));
+    false_alarms_map=zeros(7,size(patient.testingData.area,2));
+    missed_alarms_map=zeros(7,size(patient.testingData.area,2));
+    false_alarms_ml=zeros(7,size(patient.testingData.area,2));
+    missed_alarms_ml=zeros(7,size(patient.testingData.area,2));
+    for j=1:7
+        current_test_data=all_data(j,sizetesting:end);
+        for k=1:size(patient.testingData.area,2)
+            [~,idx]=ismember([current_test_data(k)],patient.training_matrix{j}(:,1),'rows');  %ML
+            if idx~=0
+                alarms_ml(j,k)=patient.training_matrix{j}(idx,4);
+                alarms_map(j,k)=patient.training_matrix{j}(idx,5);
+            end
+            if patient.testingLabels(k) == 1
+                if alarms_ml(j,k) == 0   %missed alaram ml
+                    missed_alarms_ml(j,k)=1;
+                end
+                if alarms_map(j,k) == 0  %missed alaram map
+                    missed_alarms_map(j,k)=1;
+                end
+            else
+                if alarms_ml(j,k) == 1              %false alaram ml
+                    false_alarms_ml(j,k)=1;
+                end
+                if alarms_map(j,k) == 1              %false alaram map
+                    false_alarms_map(j,k)=1;
+                end
+            end;
+        end;
+    end;
+    patient.alarms_ml=alarms_ml;
+    patient.alarms_map=alarms_map;
+    patient.false_alarms_map=false_alarms_map;
+    patient.false_alarms_ml=false_alarms_ml;
+    patient.missed_alarms_map=missed_alarms_map;
+    patient.missed_alarms_ml=missed_alarms_ml;
+    
+    
+    patients=[patients,patient];
+    
 end
 
 
@@ -177,78 +178,84 @@ all_p_dat=zeros(7,0);
 for i = 1:NUM_PATIENTS
     % Calculate H0 and H1.
     
-    figure
+    if PLOT_PRE_TASK3_FIGURES == 1
+        figure
+    end
     for j = 1:7
         HT_table_array(i, j) = {patients(i).training_matrix{j}};
         etable=zeros(2,3);
         etable(1,1)=sum(patients(i).false_alarms_ml(j,:))/(size(patients(i).testingLabels,2)-sum(patients(i).testingLabels));
         etable(1,2)=sum(patients(i).missed_alarms_ml(j,:))/(sum(patients(i).testingLabels));
         etable(1,3)=(sum(patients(i).false_alarms_ml(j,:))+sum(patients(i).missed_alarms_ml(j,:)))/size(patients(i).testingLabels,2);
-       
+        
         etable(2,1)=sum(patients(i).false_alarms_map(j,:))/(size(patients(i).testingLabels,2)-sum(patients(i).testingLabels));
         etable(2,2)=sum(patients(i).missed_alarms_map(j,:))/(sum(patients(i).testingLabels));
         etable(2,3)=(sum(patients(i).false_alarms_map(j,:))+sum(patients(i).missed_alarms_map(j,:)))/(size(patients(i).testingLabels,2));
-
+        
         Error_table_array(i, j)={etable};
         all_map_err(size(all_map_err,1)+1,:)={i,j,etable(2,3)};
-         all_ml_err(size(all_ml_err,1)+1,:)={i,j,etable(1,3)};
+        all_ml_err(size(all_ml_err,1)+1,:)={i,j,etable(1,3)};
         
+        if PLOT_PRE_TASK3_FIGURES == 1
             %h0
-       subplot(7,1,j)
-       plot(patients(i).training_matrix{j}(:,1),patients(i).training_matrix{j}(:,2));
-        %h1
-        hold on
-       plot(patients(i).training_matrix{j}(:,1),patients(i).training_matrix{j}(:,3));
-        
-        switch j
-            case 1
-                title('Mean Area under the heart beat');
-            case 2
-                title('Mean R-to-R peak interval');
-            case 3
-                title('Number of beats per minute');
-            case 4
-                title('Peak to peak interval for blood pressure');
-            case 5
-                title('Systolic Blood Pressure');
-            case 6
-                title('Diastolic Blood Pressure');
-            case 7
-                title('Pulse Pressure');                  
-        end      
-        legend('H1 pmf','H0 pmf'); 
+            subplot(7,1,j)
+            plot(patients(i).training_matrix{j}(:,1), ...
+                patients(i).training_matrix{j}(:,2), 'r');
+            %h1
+            hold on
+            plot(patients(i).training_matrix{j}(:,1), ...
+                patients(i).training_matrix{j}(:,3), 'b');
+            
+            switch j
+                case 1
+                    title('Mean Area under the heart beat');
+                case 2
+                    title('Mean R-to-R peak interval');
+                case 3
+                    title('Number of beats per minute');
+                case 4
+                    title('Peak to peak interval for blood pressure');
+                case 5
+                    title('Systolic Blood Pressure');
+                case 6
+                    title('Diastolic Blood Pressure');
+                case 7
+                    title('Pulse Pressure');
+            end
+            legend('H1 pmf','H0 pmf');
+        end % PLOT_PRE_TASK3_FIGURES == 1
     end
     % H0 is the probability that there is no patient abnomality
-      all_p_dat=[all_p_dat,patients(i).all];
+    all_p_dat=[all_p_dat,patients(i).all];
     % Tabulate a feature to get its frequency data.
-   for j=1:7
-       for k=j:7
-           if k~=j           
-             
-        corr1 = corrcoef(patients(i).all(j,:), patients(i).all(k,:));
-                 all_comb_corr(size(all_comb_corr,1)+1,:)={i,j,k,corr1(1,2)};
-
-        %fprintf('Patient %i, Properties %i %i : %f \n',i,j,k, corr1(1,2));
-           end;
-       end;
-   end;
-   %fprintf('\n\n\n\n');
+    for j=1:7
+        for k=j:7
+            if k~=j
+                
+                corr1 = corrcoef(patients(i).all(j,:), patients(i).all(k,:));
+                all_comb_corr(size(all_comb_corr,1)+1,:)={i,j,k,corr1(1,2)};
+                
+                %fprintf('Patient %i, Properties %i %i : %f \n',i,j,k, corr1(1,2));
+            end;
+        end;
+    end;
+    %fprintf('\n\n\n\n');
 end % i to NUM_PATIENTS
 %fprintf('ML\n');
 
- for j=1:7
-       for k=j:7
-           if k~=j           
-             
-        corr1 = corrcoef(all_p_dat(j,:), all_p_dat(k,:));
-                 all_comb_corr2(size(all_comb_corr2,1)+1,:)={j,k,corr1(1,2)};
+for j=1:7
+    for k=j:7
+        if k~=j
+            
+            corr1 = corrcoef(all_p_dat(j,:), all_p_dat(k,:));
+            all_comb_corr2(size(all_comb_corr2,1)+1,:)={j,k,corr1(1,2)};
+            
+            % fprintf('Patient %i, Properties %i %i : %f \n',i,j,k, corr1(1,2));
+        end;
+    end;
+end;
 
-      % fprintf('Patient %i, Properties %i %i : %f \n',i,j,k, corr1(1,2));
-           end;
-       end;
-   end;
-   
-   
+
 all_ml_err=sortrows(all_ml_err,3);
 %fprintf('MAP\n');
 all_map_err=sortrows(all_map_err,3);
